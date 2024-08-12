3f5a8cf19c2d68beca9d0fab9adffec9093dd947
YakovDronov <77423384+YakovDronov@users.noreply.github.com>
2024-08-12 22:48:10 +0700

    Added Redux

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -40,7 +40,7 @@ function MainScreen({offers}: MainScreenProps): JSX.Element {
             <div className="cities__places-container container">
               <section className="cities__places places">
                 <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{offers.length} places to stay in {activeCity.name}</b>
+                <b className="places__found">{cityOffers.length} places to stay in {activeCity.name}</b>
                 <form className="places__sorting" action="#" method="get">
                   <span className="places__sorting-caption">Sort by</span>
                   <span className="places__sorting-type" tabIndex={0}>
